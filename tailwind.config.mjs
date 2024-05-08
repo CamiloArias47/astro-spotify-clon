bc610b1a478cd8f4ac3f95461f6c47ee51b10b29
Camilo Arias <camillo47@gmail.com>
2024-05-08 18:33:12 -0500

    add grid

---
+++ tailwind.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+		colors: {
+			'base-layout': '#121212'
+		}
+	},
+	plugins: [],
+}

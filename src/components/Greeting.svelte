dbc8b200bafb2cf558e979662074b3ecc4f87570
Camilo Arias <camillo47@gmail.com>
2024-05-19 17:42:23 -0500

    add player footer

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  const currentTime = new Date()
+  const currentHour = currentTime.getHours()
+
+  let greeting = ""
+
+  if(currentHour > 12){
+    greeting = "Buenos días"
+  }
+  else if(currentHour < 18){
+    greeting = "Buenas Tardes"
+  }
+  else{
+    greeting = "Buenas Noches"
+  }
+</script>
+
+<style>
+    .greetings{
+		color: white;
+	}
+</style>
+
+<h1 class="greetings text-4xl font-bold">{greeting}</h1>
